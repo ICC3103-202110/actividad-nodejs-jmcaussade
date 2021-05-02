45014cb28069b502cb9bd51432ddbef38e13b7b2
jmcaussade <jmcaussade@miuandes.cl>
2021-05-02 16:13:19 -0400

    fibonacci without user input

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-//test commits en github
+
+function Fibonacci(number){
+    if (number<=1)
+        return number;
+    return Fibonacci(number-1) + Fibonacci(number-2);
+}
+
+var x=Fibonacci(3);
+console.log("Fibonacci of " + 1 + " is " + x)
